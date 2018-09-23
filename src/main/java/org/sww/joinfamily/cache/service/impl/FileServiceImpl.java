36f6febd8c9667687a65f90cf71f0b8181cdfe58
Shen weiwei <shenweiwei9@163.com>
2018-09-23 14:40:56 +0800

    Init

--- src/main/java/org/sww/joinfamily/cache/service/impl/FileServiceImpl.java
+++ src/main/java/org/sww/joinfamily/cache/service/impl/FileServiceImpl.java
@@ -1,9 +1,7 @@
 package org.sww.joinfamily.cache.service.impl;
 
-import java.io.Serializable;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,14 +12,14 @@ import org.sww.joinfamily.cache.service.FileService;
 public class FileServiceImpl implements FileService {
 	// private static final Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
 	@Autowired
-	private RedisTemplate<String, Serializable> redisCacheTemplate;
+	private StringRedisTemplate stringRedisTemplate;
 
 	@Override
 	public void saveToLocal(MultipartFile file, String fileType) {
 	}
 	@Override
 	public void saveToRedis() {
-		redisCacheTemplate.opsForValue().set("k1", "v1");
+		stringRedisTemplate.opsForValue().set("k1", "v1");
 	}
 	@Override
 	public void saveToDB() {
