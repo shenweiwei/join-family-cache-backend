68e567faa97d426c6ffcf142ae4f69fe712212c3
Shen weiwei <shenweiwei9@163.com>
2018-09-24 21:41:41 +0800

    init

--- src/main/java/org/sww/joinfamily/cache/config/RedisConfig.java
+++ src/main/java/org/sww/joinfamily/cache/config/RedisConfig.java
@@ -1,8 +1,9 @@
 package org.sww.joinfamily.cache.config;
 
+import java.io.Serializable;
+
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,35 +15,37 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * 主从哨兵结构不能与集群结构混用
+ * @author Administrator
+ *
+ */
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
-public class RedisConfig extends CachingConfigurerSupport {
+public class RedisConfig {
 
 	/**
-     * 配置自定义redisTemplate
-     * @return
-     */
-    @Bean
-    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        serializer.setObjectMapper(mapper);
-
-        template.setValueSerializer(serializer);
-        //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(serializer);
-        template.afterPropertiesSet();
-        return template;
-    }
-
+	 * 配置自定义redisTemplate
+	 * @return
+	 */
+	@Bean
+	public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		template.setEnableTransactionSupport(true);
+		// 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+		Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		serializer.setObjectMapper(mapper);
+		template.setValueSerializer(serializer);
+		// 使用StringRedisSerializer来序列化和反序列化redis的key值
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(serializer);
+		template.afterPropertiesSet();
+		return template;
+	}
+	
 }
--- src/main/java/org/sww/joinfamily/cache/service/impl/FileServiceImpl.java
+++ src/main/java/org/sww/joinfamily/cache/service/impl/FileServiceImpl.java
@@ -1,7 +1,10 @@
 package org.sww.joinfamily.cache.service.impl;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,16 +13,19 @@ import org.sww.joinfamily.cache.service.FileService;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class FileServiceImpl implements FileService {
-	// private static final Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
 	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+	private RedisTemplate<String, Serializable> redisTemplate;
+	
+	@Autowired
+	private LettuceConnectionFactory lettuceConnectionFactory;
 
 	@Override
 	public void saveToLocal(MultipartFile file, String fileType) {
 	}
 	@Override
 	public void saveToRedis() {
-		stringRedisTemplate.opsForValue().set("k1", "v1");
+		lettuceConnectionFactory.getClusterConfiguration();
+		redisTemplate.opsForValue().set("k1", "v1");
 	}
 	@Override
 	public void saveToDB() {
