7edf629e7d8c767c09d4191dd3858c80bcdb57eb
shenweiwei <shenweiwei9@163.com>
2018-09-27 12:40:03 +0800

    init

---
+++ src/main/java/org/sww/joinfamily/cache/pojo/ReturnTemplate.java
@@ -0,0 +1,23 @@
+package org.sww.joinfamily.cache.pojo;
+
+public class ReturnTemplate {
+	private String message;
+	private short code;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public short getCode() {
+		return code;
+	}
+
+	public void setCode(short code) {
+		this.code = code;
+	}
+
+}
