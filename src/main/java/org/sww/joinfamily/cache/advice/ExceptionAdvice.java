e8d79f0ef3003d85588137adeff662cb1200ed19
shenweiwei <shenweiwei9@163.com>
2018-09-27 14:42:52 +0800

    init

--- src/main/java/org/sww/joinfamily/cache/advice/ExceptionAdvice.java
+++ src/main/java/org/sww/joinfamily/cache/advice/ExceptionAdvice.java
@@ -3,7 +3,7 @@ package org.sww.joinfamily.cache.advice;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 	
 }
